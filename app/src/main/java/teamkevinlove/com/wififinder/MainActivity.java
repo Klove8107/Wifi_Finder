21262281f27dcfefef6cee86a85f482ae48b0734
Kevin <Klove8107@gmail.com>
2016-04-18 23:14:59 -0500

    Beginning of Working Application

--- app/src/main/java/teamkevinlove/com/wififinder/MainActivity.java
+++ app/src/main/java/teamkevinlove/com/wififinder/MainActivity.java
@@ -1,40 +1,114 @@
 package teamkevinlove.com.wififinder;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.ProgressBar;
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.List;
 
 
-    @Bind(R.id.progressBar)
-    ProgressBar mProgressBar;
-    @Bind(R.id.searchButton)
-    Button mSearchButton;
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar mProgressBar;
+    private Button mSearchButton;
+    private ListView lv;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private TextView textView;
 
 
-
-
-
-
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        textView = (TextView) findViewById(R.id.locationView);
+        mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
         mProgressBar.setVisibility(View.INVISIBLE);
-        mSearchButton = (Button)findViewById(R.id.searchButton);
-        mSearchButton.setOnClickListener(this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                textView.append(" " + location.getLongitude() + " " +location.getLatitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET
+            }, 10);
+
+        }else{
+            OnClickButtonListener();
+        }
+
     }
 
-    private void toggleRefresh() {
+        public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults){
+            switch (requestCode){
+                case 10:
+                    if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                        OnClickButtonListener();
+
+            }
+        }
+
+
+
+       public void OnClickButtonListener(){
+
+        mSearchButton = (Button)findViewById(R.id.searchButton);
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleRefresh();
+                textView.setText("LOCATION:");
+                locationManager.requestLocationUpdates("gps", 3000, 5, locationListener);
+
+            }
+        });
+    }
+
+      public void toggleRefresh() {
+
+        mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.INVISIBLE);
+
         if (mProgressBar.getVisibility() == View.INVISIBLE) {
             mProgressBar.setVisibility(View.VISIBLE);
 
@@ -44,17 +118,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void mSearchButtonClick(){
-        startActivity(new Intent("teamkevinlove.com.wififinder.NetworkInformation"));
-    }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.searchButton:
-                mSearchButtonClick();
-                break;
-        }
-    }
+
 }
 
