23b164b50f9b330501a93cb0b3ed2612e0167713
Kevin <Klove8107@gmail.com>
2016-04-19 19:29:35 -0500

    Finished Wifi Portion of project

--- app/src/main/java/teamkevinlove/com/wififinder/MainActivity.java
+++ app/src/main/java/teamkevinlove/com/wififinder/MainActivity.java
@@ -1,9 +1,12 @@
 package teamkevinlove.com.wififinder;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -11,13 +14,13 @@ import android.location.LocationManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -33,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
     private LocationManager locationManager;
     private LocationListener locationListener;
     private TextView textView;
+    WifiManager mainWifiObj;
+    WifiScanReceiver wifiReciever;
+    ListView list;
+    String wifis[];
 
 
     @TargetApi(Build.VERSION_CODES.M)
@@ -40,6 +47,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = (ListView)findViewById(R.id.listView);
+        mainWifiObj = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        wifiReciever = new WifiScanReceiver();
+        mainWifiObj.startScan();
         textView = (TextView) findViewById(R.id.locationView);
         mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
         mProgressBar.setVisibility(View.INVISIBLE);
@@ -47,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
+                textView.setText("LOCATION:");
                 textView.append(" " + location.getLongitude() + " " +location.getLatitude());
             }
 
@@ -68,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         };
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
                 (this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(new String[]{
+            requestPermissions(new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,
                     Manifest.permission.INTERNET
             }, 10);
@@ -76,21 +88,21 @@ public class MainActivity extends AppCompatActivity {
         }else{
             OnClickButtonListener();
         }
-
+        mProgressBar.setVisibility(View.INVISIBLE);
     }
 
-        public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults){
-            switch (requestCode){
-                case 10:
-                    if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                        OnClickButtonListener();
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode){
+            case 10:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    OnClickButtonListener();
 
-            }
         }
+    }
 
 
 
-       public void OnClickButtonListener(){
+    public void OnClickButtonListener(){
 
         mSearchButton = (Button)findViewById(R.id.searchButton);
         mSearchButton.setOnClickListener(new View.OnClickListener() {
@@ -98,13 +110,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 toggleRefresh();
                 textView.setText("LOCATION:");
-                locationManager.requestLocationUpdates("gps", 3000, 5, locationListener);
+                locationManager.requestLocationUpdates(locationManager.NETWORK_PROVIDER, 30000, 5, locationListener);
+                textView.setText("LOCATION:");
+                locationManager.requestLocationUpdates("gps", 20000, 5, locationListener);
+                mProgressBar.setVisibility(View.INVISIBLE);
 
             }
         });
     }
 
-      public void toggleRefresh() {
+    public void toggleRefresh() {
 
         mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
         mProgressBar.setVisibility(View.INVISIBLE);
@@ -119,6 +134,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+    class WifiScanReceiver extends BroadcastReceiver {
+        @SuppressLint("UseValueOf")
+        public void onReceive(Context c, Intent intent) {
+            List<ScanResult> wifiScanList = mainWifiObj.getScanResults();
+            wifis = new String[wifiScanList.size()];
+            for(int i = 0; i < wifiScanList.size(); i++){
+                wifis[i] = ((wifiScanList.get(i)).toString());
+            }
+            String filtered[] = new String[wifiScanList.size()];
+            int counter = 0;
+            for (String eachWifi : wifis) {
+                String[] temp = eachWifi.split(",");
+
+                filtered[counter] = temp[0].substring(5).trim()+"\n" + temp[2].substring(12).trim()+"\n" +temp[3].substring(6).trim();//0->SSID, 2->Key Management 3-> Strength
+
+
+                counter++;
+
+            }
+            list.setAdapter(new ArrayAdapter<String>(getApplicationContext(),
+                    R.layout.list_black_layout,R.id.list_content, wifis));
+
+
+
+        }
+    }
+    protected void onPause() {
+        unregisterReceiver(wifiReciever);
+        super.onPause();
+    }
+
+    protected void onResume() {
+        registerReceiver(wifiReciever, new IntentFilter(
+                WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        super.onResume();
+
+    }
 
 }
 
